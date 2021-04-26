fd57d12fe6a3c546bf76a68f11b587de60a01f3a
RenanAiresTIC <renan.aires.tic@gmail.com>
2021-04-26 19:02:08 -0300

    :tada: desafio 01 concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle)     
+      return;
+    
+    let id = Math.floor((1 + Math.random()) * 0x10000);
+       
+    const newTask: Task = {
+      id: id,
+      title: newTaskTitle,
+      isComplete:false      
+    }
+    
+    setTasks([...tasks, newTask]);  
+    setNewTaskTitle('');
   }
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskComplete = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete:  !task.isComplete
+    } : task);
+
+    setTasks(taskComplete);        
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id)
+      setTasks(filteredTask)
   }
 
   return (
@@ -38,6 +60,7 @@ export function TaskList() {
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
+          
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
           </button>
